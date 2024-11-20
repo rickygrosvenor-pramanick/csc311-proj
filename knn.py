61d0d874780fc15a15041e25009eeeb095516f84
Gerald Wang <geraldyywang@gmail.com>
2024-11-20 14:51:25 -0500

    finishes Q1

--- knn.py
+++ knn.py
@@ -26,7 +26,7 @@ def knn_impute_by_user(matrix, valid_data, k):
     # We use NaN-Euclidean distance measure.
     mat = nbrs.fit_transform(matrix)
     acc = sparse_matrix_evaluate(valid_data, mat)
-    print("Validation Accuracy: {}".format(acc))
+    print("Validation Accuracy (user), k={}: {}".format(k, acc))
     return acc
 
 
@@ -41,10 +41,17 @@ def knn_impute_by_item(matrix, valid_data, k):
     :return: float
     """
     #####################################################################
-    # TODO:                                                             #
     # Implement the function as described in the docstring.             #
     #####################################################################
-    acc = None
+    transposed_matrix = matrix.T
+    nbrs = KNNImputer(n_neighbors=k)
+
+    imputed_matrix = nbrs.fit_transform(transposed_matrix)
+    restored_matrix = imputed_matrix.T
+
+    acc = sparse_matrix_evaluate(valid_data, restored_matrix)
+    print("Validation Accuracy (item), k={}: {}".format(k, acc))
+
     #####################################################################
     #                       END OF YOUR CODE                            #
     #####################################################################
@@ -62,12 +69,60 @@ def main():
     print(sparse_matrix.shape)
 
     #####################################################################
-    # TODO:                                                             #
     # Compute the validation accuracy for each k. Then pick k* with     #
     # the best performance and report the test accuracy with the        #
     # chosen k*.                                                        #
     #####################################################################
-    pass
+    k_vals = [1, 6, 11, 16, 21, 26]
+    max_k_user_acc, max_k_item_acc = -float("inf"), -float("inf")
+    max_k_user, max_k_item = -1, -1
+    user_accuracies = []
+    item_accuracies = []
+
+    for k in k_vals:
+        curr_k_user_acc = knn_impute_by_user(sparse_matrix, val_data, k)
+        curr_k_item_acc = knn_impute_by_item(sparse_matrix, val_data, k)
+        user_accuracies.append(curr_k_user_acc)
+        item_accuracies.append(curr_k_item_acc)
+
+        if curr_k_user_acc > max_k_user_acc:
+            max_k_user_acc = curr_k_user_acc
+            max_k_user = k
+
+        if curr_k_item_acc > max_k_item_acc:
+            max_k_item_acc = curr_k_item_acc
+            max_k_item = k
+
+    user_test_acc_k_star = knn_impute_by_user(sparse_matrix, test_data, max_k_user)
+    item_test_acc_k_star = knn_impute_by_item(sparse_matrix, test_data, max_k_item)
+    print(f"User KNN test accuracy for k*={max_k_user}: {user_test_acc_k_star}")
+    print(f"Item KNN test accuracy for k*={max_k_item}: {item_test_acc_k_star}")
+
+    plt.figure(figsize=(10, 6))
+    plt.plot(k_vals, user_accuracies, label="User-based KNN Accuracy", marker="o")
+    plt.plot(k_vals, item_accuracies, label="Item-based KNN Accuracy", marker="s")
+
+    plt.scatter(
+        [max_k_user],
+        [max_k_user_acc],
+        color="blue",
+        label=f"Max User Accuracy (k*={max_k_user})",
+        zorder=5,
+    )
+    plt.scatter(
+        [max_k_item],
+        [max_k_item_acc],
+        color="red",
+        label=f"Max Item Accuracy (k*={max_k_item})",
+        zorder=5,
+    )
+
+    plt.title("KNN Accuracy vs. Number of Neighbors (k)")
+    plt.xlabel("Number of Neighbors (k)")
+    plt.ylabel("Accuracy")
+    plt.legend()
+    plt.grid()
+    plt.show()
     #####################################################################
     #                       END OF YOUR CODE                            #
     #####################################################################
